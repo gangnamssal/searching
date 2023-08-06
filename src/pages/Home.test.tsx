0205f84c87bcea05a4a4059649bc72dd02e145f2
ggnsall <ggnsall@naver.com>
2023-08-06 21:41:18 +0900

    [feat] Home 컴포넌트 테스트코드 작성 완료

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,9 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
 import Home from './Home';
 
 describe('Home page', () => {
-  it('is render', () => {
+  it('has title', () => {
     render(<Home />);
+
+    const firstTitleEl = screen.getByText(/국내 모든 임상시험 검색하고/);
+    const secondTitleEl = screen.getByText(/온라인으로 참여하기/);
+
+    expect(firstTitleEl).toBeInTheDocument();
+    expect(secondTitleEl).toBeInTheDocument();
   });
 });
--- src/vite-setup.ts
+++
@@ -1,7 +0,0 @@
-// src/setupTests.ts
-import { server } from './mocks/server.ts';
-import { afterAll, afterEach, beforeAll } from 'vitest';
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
